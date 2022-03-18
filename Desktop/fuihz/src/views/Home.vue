293288a8d61c8ddca9d8e74d679efeefe70c4a41
clownfish88 <292271013@qq.com>
2022-03-18 19:54:49 +0800

    学习提交

---
+++ Desktop/fuihz/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <h1>
+          首页
+      </h1>
+      <div class="top">
+          <div class="logo"></div>
+          <div class="top-search">
+
+          </div>
+          <div class="top-user">
+
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            
+        }
+    },
+    
+
+}
+</script>
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ Desktop/fuihz/src/views/home.js
@@ -0,0 +1,9 @@
+// function showCount(count) {
+//     // 如果 count 为 undefined 或 null，则提示 "unknown"
+//     alert(count ?? "unknown");
+//   }
+  
+//   showCount(0); // 0
+//   showCount(null); // unknown
+//   showCount(); // unknown
+
